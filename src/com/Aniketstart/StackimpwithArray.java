b3ad1b735fc4405ff249e02b6c6ba96c0365b23e
Aniket Kailas Ghavte <aniketghavte@gmail.com>
2022-06-04 19:28:45 +0530

    Stack implementation with arraylist

---
+++ src/com/Aniketstart/StackimpwithArray.java
@@ -0,0 +1,50 @@
+package com.Aniketstart;
+
+import java.util.ArrayList;
+
+class MyStack {
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public boolean isEmpty(){
+        return list.size() == 0;
+    }
+
+    public void push(int data){
+        list.add(data);
+    }
+
+    public int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
+
+    public int peek(){
+
+        if (isEmpty()){
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+
+}
+public class StackimpwithArray {
+
+    public static void main(String[] args) {
+
+        MyStack s =new MyStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.pop();
+
+        while (!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
